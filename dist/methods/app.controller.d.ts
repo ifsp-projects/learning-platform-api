3694c128d985f694d30da19e95e4e87f310128c9
Vitor Gabriel Silva - PESSOAL <vitorgabrielsbo1460@gmail.com>
2024-09-07 15:02:40 -0300

    fix(.github) adjusted folders

--- dist/methods/app.controller.d.ts
+++
@@ -1,6 +0,0 @@
-import { PrismaService } from '../app.service';
-export declare class AppController {
-    private readonly appService;
-    constructor(appService: PrismaService);
-    getHello(): string;
-}
--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,7 +1,5 @@
 import { PrismaClient } from '@prisma/client'
 
-
-//PrismaClient
 export const prisma = new PrismaClient({ 
     log: ['query']
 }); 
