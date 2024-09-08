6e9c986fdc16bebcfd5e4299d15bf6b9c393ec50
Vitor Gabriel Silva - PESSOAL <vitorgabrielsbo1460@gmail.com>
2024-09-08 00:15:03 -0300

    feat(yarn.lock) added new dependencies

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,6 @@
 import { PrismaClient } from '@prisma/client'
+import { IS_DEVELOP_MODE } from '../constants/environments/isDevelopMode'
 
-export const prisma = new PrismaClient({ 
-    log: ['query']
-}); 
+export const prisma = new PrismaClient({
+  log: IS_DEVELOP_MODE ? ['query'] : []
+})
