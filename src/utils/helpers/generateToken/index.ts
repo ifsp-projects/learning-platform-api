2a99ae64157fa1066e81b3c5c0cea72ab4bd737f
Vitor Gabriel Silva - PESSOAL <vitorgabrielsbo1460@gmail.com>
2024-09-22 00:26:26 -0300

    fix(project) removed vulnerabilities

--- src/utils/helpers/generateToken/index.ts
+++
@@ -1,7 +0,0 @@
-const getRandomString = () => Math.random().toString(36).substring(2)
-
-export function generateToken(length: number) {
-  const token = getRandomString() + getRandomString()
-
-  return token.substring(0, length)
-}
